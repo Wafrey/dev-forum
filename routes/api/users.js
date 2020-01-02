11c50fa01c7a306585e1571f74812d97d54a9b87
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-01-02 21:30:45 +0200

    add email, password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -54,4 +54,34 @@ router.post('/register', (req, res) => {
     })
 });
 
+// @route GET api/users/login
+// @desc Login user / Returning Json web token
+// @access Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({
+        email: email,
+    }).then(user => {
+        // Check for user
+        if (!user) {
+            return res.status(404).json({
+                email: 'User not found in db'
+            });
+        }
+
+        // Check Password
+        bcrypt.compare(password, user.password)
+            .then(isMatch=>{
+                if (isMatch) {
+                    res.json({msg: 'Success'});
+                }else{
+                    return res.status(400).json({password: 'Password or username incorrect'});
+                }
+            })
+    });
+});
+
 module.exports = router;
