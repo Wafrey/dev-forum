09b945a75cebbf4686d59890d8081ff52da1303b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-01-03 21:38:46 +0200

    creating jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:admin123@devconnector-oo5eq.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://admin:admin123@devconnector-oo5eq.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: 'secret'
 }
