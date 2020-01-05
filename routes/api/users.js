f0355d1fa5e56adca13ea6109fd7155a7f04a54b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-01-05 21:47:44 +0200

    validation handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -20,6 +23,17 @@ router.get('/test', (req, res) => res.json({
 // @desc Register user
 // @access Public
 router.post('/register', (req, res) => {
+
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+ 
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({
         email: req.body.email
     }).then(user => {
