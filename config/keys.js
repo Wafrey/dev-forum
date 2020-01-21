c0a0bb27f68aca976868b453a956121ec35d7a24
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-01-21 21:31:40 +0200

    updated passport js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://admin:admin123@devconnector-oo5eq.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: 'secret'
+    secretOrKey: 'secret',
+    useNewUrlParser: true 
 }
