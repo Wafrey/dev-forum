1a0c9bd84b72a912f5c367cb304b642b5bd45f4b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-01-27 21:33:41 +0200

    add react, and navbar component, footer component, and landing component

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; {new Date().getFullYear()} DevForum
+      </footer>
+    )
+}
