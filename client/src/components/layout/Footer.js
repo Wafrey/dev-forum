0c12ea0b3bf1ebf229dc897fa8000394e75e0e08
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-01-29 22:16:05 +0200

    update react register form with state

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => {
     return (
-        <footer class="bg-dark text-white mt-5 p-4 text-center">
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
         Copyright &copy; {new Date().getFullYear()} DevForum
       </footer>
     )
