1cd3183c3441dde619a04514c3b4aa4788558230
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-02-07 22:49:05 +0200

    redux login set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./../utils/setAuthToken";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -7,4 +9,35 @@ export const registerUser = (userData, history) => dispatch => {
     .post("/api/users/register", userData)
     .then(res => history.push("/login"))
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+
+      // Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+
+      // Set token to Auth header
+      setAuthToken(token);
+
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+// Set logged in user
+export const setCurrentUser = decode => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decode
+  };
 };
