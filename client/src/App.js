89b8ae8e63bd70c21515e2e239f481bc763a5cbd
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-02-13 21:49:15 +0200

    created create-profile component, and it route

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 
@@ -56,6 +57,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
