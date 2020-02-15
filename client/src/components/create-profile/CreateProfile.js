e9ebb73dec8a7dba5bcf5120828419319bcea4d4
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-02-15 20:32:34 +0200

    finish with create profile Functionality

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import TextFieldGroup from "./../common/TextFieldGroup";
 import TextAreaFieldGroup from "./../common/TextAreaFieldGroup";
 import InputGroup from "./../common/InputGroup";
 import SelectListGroup from "./../common/SelectListGroup";
+import { createProfile } from "../../actions/profileActions";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -29,10 +31,32 @@ class CreateProfile extends Component {
     };
   }
 
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit = e => {
     e.preventDefault();
 
-    console.log("submit");
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    };
+
+    this.props.createProfile(profileData, this.props.history);
   };
 
   onChange = e => {
@@ -40,6 +64,7 @@ class CreateProfile extends Component {
 
     this.setState({ [e.target.name]: e.target.value });
   };
+
   render() {
     const { errors, displaySocialInputs } = this.state;
 
@@ -221,4 +246,6 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(
+  withRouter(CreateProfile)
+);
