1e075610703d58420a641542512bbd9d38c5252f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-02-19 21:47:08 +0200

    finish with addeducation functionality

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 import "./App.css";
 
@@ -79,6 +80,13 @@ class App extends Component {
                   exact
                   path="/add-experience"
                   component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
