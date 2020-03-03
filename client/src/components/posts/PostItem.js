7b7552578b002e09bfe66df5c78c9f208295d26e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-03-03 21:24:55 +0200

    finish with postfeed

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PostItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import PostForm from './PostForm';
-import Spinner from '../common/Spinner';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import PostForm from "./PostForm";
+import { getPosts } from "../../actions/postActions";
+import Spinner from "../common/Spinner";
+import PostFeed from "./PostFeed";
 
 class Posts extends Component {
- 
-
+  componentDidMount() {
+    this.props.getPosts();
+  }
   render() {
     const { posts, loading } = this.props.post;
-  
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -26,11 +35,12 @@ class Posts extends Component {
 }
 
 Posts.propTypes = {
-  post: PropTypes.object.isRequired
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   post: state.post
 });
 
-export default connect(mapStateToProps, { })(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
