31f975c5f69d964f2a252646df00a5385ef976b8
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-03-03 12:45:52 +0200

    add profile-github fetching data from the githubapi, and finish with profile display

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,6 +16,12 @@ class Profile extends Component {
     }
   }
 
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push("/not-found");
+    }
+  }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
