b6a6df71f32931c285065693110363285a996bc6
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-03-03 13:33:15 +0200

    add addpost action func, and post state

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  post: [],
+  post: {},
+  loading: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
