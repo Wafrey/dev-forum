6c995606753cd983da2d717c1d314c82e8e44e75
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-05-04 16:20:45 +0300

    update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: "mongodb+srv://admin:admin123@devconnector-oo5eq.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: 'secret',
-    useNewUrlParser: true 
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
 }
